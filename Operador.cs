ee44b888b7b5913c34984298ad0119a305114797
IRivera99 <ignacio.j.rivera.99@gmail.com>
2023-10-21 01:14:42 -0300

    Initial commit

---
+++ Operador.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    abstract class Operador
+    {
+        int id;        
+        int capacidadBateria;
+        int mAhActuales;
+        bool standby; //true= OPERATIVO, false=STANDBY
+        int cargaMaxima;
+        int cargaActual;
+        double velocidadOptima;
+        double velocidadActual;
+        string localizacion;
+
+        protected Operador(int id, int capacidadBateria, int mAhActuales, int cargaMaxima, double velocidadOptima, string localizacion)
+        {
+            this.id = id;
+            this.capacidadBateria = capacidadBateria;            
+            this.mAhActuales = mAhActuales;
+            standby = false;
+            this.cargaMaxima = cargaMaxima;
+            cargaActual = 0;
+            this.velocidadOptima = velocidadOptima;
+            velocidadActual = CalcularVelocidadActual(CalcularNivelBateria(this.mAhActuales));
+            this.localizacion = localizacion.ToUpper();            
+        }
+
+        protected Operador(int id)
+        {
+            this.id = id;
+            capacidadBateria = 0;
+            mAhActuales = 0;
+            cargaMaxima = 0;
+            cargaActual = 0;
+            velocidadOptima = 0;
+            velocidadActual = CalcularVelocidadActual(mAhActuales);
+            localizacion = string.Empty;
+        }
+
+        protected double CalcularVelocidadActual(int porcentajeBateria)
+        {
+            double velocidad = velocidadOptima;
+
+            for (int i = (porcentajeBateria - 100); i <= -10; i += 10)
+            {
+                velocidad -= velocidadOptima * 0.05;
+            }
+
+            return velocidad;
+        }    
+        
+        protected int CalcularNivelBateria(int mAh)
+        {
+            return Convert.ToInt32((mAh * 100) / capacidadBateria); 
+        }
+
+        protected void DescargarBateria(int mAhConsumidos)
+        {
+            if (mAhConsumidos > mAhActuales)
+                mAhActuales = 0;
+            else
+                mAhActuales -= mAhConsumidos;
+
+            velocidadActual = CalcularVelocidadActual(CalcularNivelBateria(this.mAhActuales));
+        }
+
+        protected void CargarBatería(int mAhAñadidos)
+        {
+            if ((mAhAñadidos + mAhActuales) > capacidadBateria)
+                mAhActuales = capacidadBateria;
+            else
+                mAhActuales += mAhAñadidos;
+
+            velocidadActual = CalcularVelocidadActual(CalcularNivelBateria(this.mAhActuales));
+        }
+
+        protected bool AñadirCargaFisica(int kilos)
+        {
+            bool añadido = false;
+
+            if (kilos < cargaMaxima && (kilos + cargaActual) < cargaMaxima)
+            {
+                cargaActual += kilos;
+                añadido = true;
+            }
+
+            return añadido;
+        }
+
+        protected void DescargarCargaFisica(int kilos)
+        {
+            if(kilos > cargaActual)
+                cargaActual = 0;
+            else
+                cargaActual -= kilos;
+        }
+
+        public bool Moverse(string localizacion, int kilometrosARecorrer)
+        {
+            bool seMovio = false;
+            int mAhConsumidos = 0;
+            int nivelBateria = CalcularNivelBateria(mAhActuales);
+            double velocidad = velocidadActual;
+            int mAhBajaVelocidad = Convert.ToInt32(capacidadBateria / 100); //Cada cuantos mAh se me baja la velocidad
+            double tiempoBajaVelocidad = Math.Round((mAhBajaVelocidad * 0.001), 2); //Cada cuanto tiempo me baja la velocidad          
+            
+            if (localizacion.ToUpper().Equals("CUARTEL"))
+            {
+                seMovio = true;
+                kilometrosARecorrer = 0;
+            }
+
+            while(kilometrosARecorrer > 0 && nivelBateria > 0)
+            {
+                kilometrosARecorrer -= Convert.ToInt32(tiempoBajaVelocidad * velocidad);
+                nivelBateria--;
+                mAhConsumidos += capacidadBateria / 100;
+                velocidad = CalcularVelocidadActual(nivelBateria);
+
+                if (nivelBateria > 0 && kilometrosARecorrer <= 0)
+                    seMovio = true;
+            }
+
+            if (seMovio)
+            {
+                this.localizacion = localizacion.ToUpper();
+                DescargarBateria(mAhConsumidos);
+            }                
+
+            return seMovio;
+        }
+
+        public bool TransferirBateria(Operador operadorDonatario)
+        {
+            bool transferenciaRealizada = false;
+            int bateriaFaltanteDonatorio = operadorDonatario.capacidadBateria - operadorDonatario.mAhActuales;
+
+            if (localizacion.Equals(operadorDonatario.localizacion) && !standby)
+            {
+                operadorDonatario.CargarBatería(mAhActuales);
+                DescargarBateria(bateriaFaltanteDonatorio);
+                transferenciaRealizada = true;                               
+            }            
+
+            return transferenciaRealizada;
+        }
+
+        public bool TransferirCarga(Operador operadorDonatario)
+        {
+            bool transferenciaRealizada = false;
+
+            if (localizacion.Equals(operadorDonatario.localizacion) && operadorDonatario.AñadirCargaFisica(cargaActual) && !standby)
+            {
+                DescargarCargaFisica(cargaActual);
+                transferenciaRealizada = true;
+            }
+
+            return transferenciaRealizada;
+        }
+
+        public bool VolverCuartelDescargaFisica()
+        {
+            bool vueltaYDescarga = false;
+
+            if (!standby)
+            {
+                Moverse("cuartel", 0);
+                DescargarCargaFisica(cargaActual);
+                vueltaYDescarga = true;
+            }
+
+            return vueltaYDescarga;
+        }
+
+        public bool VolverCuartelCargarBateria()
+        {
+            bool vueltaYCarga = false;
+
+            if (!standby)
+            {
+                Moverse("cuartel", 0);
+                CargarBatería((capacidadBateria - cargaActual));
+                vueltaYCarga = true;
+            }
+
+            return vueltaYCarga;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Integrador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mAhActuales = 1500;
+            int capacidadBateria = 6500;            
+            double velocidadOptima = 50;
+            double velocidadActual = CalcularVelocidadActual(CalcularNivelBateria(mAhActuales));
+
+            double CalcularVelocidadActual(int porcentajeBateria)
+            {
+                double velocidad = velocidadOptima;
+
+                for (int i = (porcentajeBateria - 100); i <= -10; i += 10)
+                {
+                    velocidad -= velocidadOptima * 0.05;
+                }
+
+                return velocidad;
+            }
+
+            int CalcularNivelBateria(int mAh)
+            {
+                return Convert.ToInt32((mAh * 100) / capacidadBateria);
+            }
+
+            void DescargarBateria(int mAhConsumidos)
+            {
+                if (mAhConsumidos > mAhActuales)
+                    mAhActuales = 0;
+                else
+                    mAhActuales -= mAhConsumidos;
+
+                velocidadActual = CalcularVelocidadActual(CalcularNivelBateria(mAhActuales));
+            }
+
+            bool Moverse(string localizacion, int kilometrosARecorrer)
+            {
+                bool seMovio = false;
+                int mAhConsumidos = 0;
+                int nivelBateria = CalcularNivelBateria(mAhActuales);
+                double velocidad = velocidadActual;                
+                int mAhBajaVelocidad = Convert.ToInt32(capacidadBateria / 100); //Cada cuantos mAh se me baja la velocidad
+                double tiempoBajaVelocidad = Math.Round((mAhBajaVelocidad * 0.001), 2); //Cada cuanto tiempo me baja la velocidad
+
+                if (localizacion.ToUpper().Equals("CUARTEL"))
+                {
+                    seMovio = true;
+                    kilometrosARecorrer = 0;
+                }
+
+                while (kilometrosARecorrer > 0 && nivelBateria > 0)
+                {
+                    kilometrosARecorrer -= Convert.ToInt32(tiempoBajaVelocidad * velocidad);
+                    nivelBateria--;
+                    mAhConsumidos += capacidadBateria / 100;
+                    velocidad = CalcularVelocidadActual(nivelBateria);
+
+                    if (nivelBateria > 0 && kilometrosARecorrer <= 0)
+                        seMovio = true;
+                }
+
+                if (seMovio)
+                {
+                    //this.localizacion = localizacion.ToUpper();
+                    DescargarBateria(mAhConsumidos);
+                }
+
+                return seMovio;
+            }
+            Console.WriteLine(Moverse("CACA", 45));
+            Console.WriteLine($"{mAhActuales}");
+            Console.WriteLine($"{velocidadActual}");
+            Console.WriteLine(Moverse("CACA", 44));
+            Console.WriteLine($"{mAhActuales}");
+            Console.WriteLine($"{velocidadActual}");
+            //int mAhBajaVelocidad = Convert.ToInt32((10 * 6500) / 100);
+            //double tiempoBajaVelocidad = Math.Round((mAhBajaVelocidad*0.001), 2);
+            //int distRecorridaAntesBaja = Convert.ToInt32(tiempoBajaVelocidad * 35);
+            //Console.WriteLine($"cada cuanto mha me baja velocidad: {mAhBajaVelocidad}mAh");
+            //Console.WriteLine($"cada cuanto tiempo me baja velocidad: {tiempoBajaVelocidad}hrs");
+            //Console.WriteLine($"cuantos km hago antes que me baje velocidad: {distRecorridaAntesBaja}kms");
+            Console.ReadLine();
+        }
+    }
+}
